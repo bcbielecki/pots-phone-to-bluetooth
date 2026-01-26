4cc6b9123764823b41a2a69cd86bb62a12ab1f70
Benjamen Bielecki <45702229+bcbielecki@users.noreply.github.com>
2026-01-25 18:22:16 -0800

    implement LED loop as separate thread

--- src/main.cpp
+++ src/main.cpp
@@ -4,9 +4,31 @@
 
 // Define the GPIO pin for the LED (GPIO 2 is common for onboard LEDs)
 #define BLINK_GPIO GPIO_NUM_2
+#define STACK_SIZE 2048
+
+void blinkLEDLoop(void * pvParameters);
 
 // extern "C" is used to prevent name mangling when using C++ compiler, so that the ESP-IDF can find the app_main function.
 extern "C" void app_main(void)
+{
+    static uint8_t ucParameterToPass;
+    TaskHandle_t xHandle = NULL;
+
+    // Create the task, storing the handle.  Note that the passed parameter ucParameterToPass
+    // must exist for the lifetime of the task, so in this case is declared static.  If it was just an
+    // an automatic stack variable it might no longer exist, or at least have been corrupted, by the time
+    // the new task attempts to access it.
+    xTaskCreate( blinkLEDLoop, "BlinkLEDTask", STACK_SIZE, &ucParameterToPass, tskIDLE_PRIORITY, &xHandle );
+        configASSERT( xHandle );
+
+    // Use the handle to delete the task.
+    // if( xHandle != NULL )
+    // {
+    //  vTaskDelete( xHandle );
+    // }
+}
+
+void blinkLEDLoop(void * pvParameters)
 {
     // Configure the GPIO pin
     gpio_reset_pin(BLINK_GPIO);
